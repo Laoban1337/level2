3ce17533bda06ddcdaea78fd984b99227a6e45d8
Brian Monestime <bmonestime@gmail.com>
2024-02-13 09:30:37 -0700

    finished

--- assignments/arrayMap/index.js
+++ assignments/arrayMap/index.js
@@ -66,36 +66,69 @@
 // console.log(returnNames(namesOnly));
 
 ///5) Make an array of strings of the names saying whether or not they can go to The Matrix
-const namesOnly = [
-  {
-    name: "Angelina Jolie",
-    age: 80,
-  },
-  {
-    name: "Eric Jones",
-    age: 2,
-  },
-  {
-    name: "Paris Hilton",
-    age: 5,
-  },
-  {
-    name: "Kayne West",
-    age: 16,
-  },
-  {
-    name: "Bob Ziroll",
-    age: 100,
-  },
-];
-function makeStrings(arr) {
-   return arr.map(function (object) {
-    if (object.age <= 18) {
-      return object.name + " Can not enter the matrix";
-    } else {
-      return object.name + " Can enter the matrix";
-    }
-  });
-}
+// const namesOnly = [
+//   {
+//     name: "Angelina Jolie",
+//     age: 80,
+//   },
+//   {
+//     name: "Eric Jones",
+//     age: 2,
+//   },
+//   {
+//     name: "Paris Hilton",
+//     age: 5,
+//   },
+//   {
+//     name: "Kayne West",
+//     age: 16,
+//   },
+//   {
+//     name: "Bob Ziroll",
+//     age: 100,
+//   },
+// ];
+// function makeStrings(arr) {
+//    return arr.map(function (object) {
+//     if (object.age <= 18) {
+//       return object.name + " Can not enter the matrix";
+//     } else {
+//       return object.name + " Can enter the matrix";
+//     }
+//   });
+// }
 
-console.log(makeStrings(namesOnly));
+// console.log(makeStrings(namesOnly));
+
+
+
+const namesOnly = [
+      {
+        name: "Angelina Jolie",
+        age: 80,
+      },
+      {
+        name: "Eric Jones",
+        age: 2,
+      },
+      {
+        name: "Paris Hilton",
+        age: 5,
+      },
+      {
+        name: "Kayne West",
+        age: 16,
+      },
+      {
+        name: "Bob Ziroll",
+        age: 100,
+      },
+    ];
+
+    function elementCreator(arrayOfObjects){
+         return arrayOfObjects.map(function(object ){
+          return `<h1>${object.name}<h1>  <h2>${object.age}<h2>`
+         })
+    }
+
+    console.log(elementCreator(namesOnly));
