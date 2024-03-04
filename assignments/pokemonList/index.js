8df54b3724311017e9a72e57e078f9b65a784bfb
Brian Monestime <bmonestime@gmail.com>
2024-03-04 09:44:36 -0700

    Pokedex displayed

---
+++ assignments/pokemonList/index.js
@@ -0,0 +1,21 @@
+// const axios = require("axios");
+
+// // Getting Data from database
+// // function getPokeData() {
+// //   axios
+// //     .get("https://api.vschool.io/pokemon")
+// //     .then((response) => displayPokeData(response.data.objects[0]))
+// //     .catch((err) => console.log("There was an error: " + err));
+// // }
+
+// // function displayPokeData(array) {
+// //   return array.map((pokemon) => {
+// //     const h1 = document.createElement("h1");
+// //     h1.textContent = pokemon.name;
+// //     document.body.appendChild(h1);
+// //     return pokemon;
+// //   });
+ 
+// // }
+
+// // getPokeData();
